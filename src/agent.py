efab9cb2b83a740377a6702ece6c2413e22d303c
ZalanBoss <zalan.bossi@gmail.com>
2024-09-17 21:04:30 +0000

    Solved the problem with rotation by Matmul

--- src/agent.py
+++ src/agent.py
@@ -4,7 +4,6 @@ from functions import rot_mat
 
 class Agent():
     def __init__(self, initial_postion, initial_orientation=np.pi/2):
-        
         # TRAITS
             # MOVEMENT
         self.position = initial_postion
@@ -15,22 +14,20 @@ class Agent():
             # DETECTION
         self.sensor_size = SEN_SIZE
         self.s1 = [np.array([0, 3+SEN_POINT_RADIUS*i]) for i in range(SEN_SIZE//SEN_POINT_RADIUS)]
-        self.s2 = [np.array([i,j])*rot_mat(np.pi/4) for (i,j) in self.s1]
-        self.s3 = [np.array([i,j])*rot_mat(-np.pi/4) for (i,j) in self.s1]
-        self.s4 = [np.array([i,j])*rot_mat(-np.pi/2) for (i,j) in self.s1]
-        self.s5 = [np.array([i,j])*rot_mat(np.pi/2) for (i,j) in self.s1]
-
+        self.s2 = [rot_mat(np.pi/4)@np.array([i,j]) for (i,j) in self.s1]
+        self.s3 = [rot_mat(-np.pi/4)@np.array([i,j]) for (i,j) in self.s1]
+        self.s4 = [rot_mat(-np.pi/2)@np.array([i,j]) for (i,j) in self.s1]
+        self.s5 = [rot_mat(np.pi/2)@np.array([i,j]) for (i,j) in self.s1]
+        print(f"[1,0] vector rotated by 90 deg is: {rot_mat(np.pi/2)@np.array([1,0])}")
         #print(f"first se.. {self.s1}")
-        
         self.sensor_positions = [self.s1,self.s2,self.s3,self.s4,self.s5]
-        print(f"s-s: {self.sensor_positions}")
+        #print(f"s-s: {self.sensor_positions}")
         # INPUTS
         self.sensors = np.array([[-1,-1,-1], [-1,-1,-1], [-1,-1,-1], [-1,-1,-1], [-1,-1,-1]])
         # OUTPUTS
         self.chromosome = np.random.uniform(-10, 10, (2,3,5))
     def update(self, dt):
         self.acceleration += self.handle_input()[0]
-        
         if self.acceleration > MAX_ACC:
             self.acceleration = MAX_ACC
         
@@ -38,9 +35,7 @@ class Agent():
         #print(f"handle_input {self.handle_input()[1]}")
         if self.angular_velocity > MAX_ANG_VEL:
             self.angular_velocity = MAX_ANG_VEL
-
         #print(f"Initial acceleration: {self.acceleration}")
-
         self.velocity += self.acceleration*dt
         self.position[0] += self.velocity*np.cos(self.orientation)*dt
         self.position[1] += self.velocity*np.sin(self.orientation)*dt
@@ -50,12 +45,8 @@ class Agent():
     def handle_input(self):
         acc = np.sum(np.matmul(self.sensors,self.chromosome[0]))
         ang_vel = np.sum(np.matmul(self.sensors,self.chromosome[1]))
-
         #print(f"{ang_vel}")
         #print(f"{self.chromosome[0].shape}")
         return (acc, ang_vel) 
     def detection(self, point):
-        
-        
-
         pass
