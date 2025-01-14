ec2a21ad3c619b196d56d9f58e9a2df4006c4bd4
Ronifiu <aron.pakozdi@gmail.com>
2025-01-14 21:03:41 +0100

    some deletions

--- src/mutation.py
+++ src/mutation.py
@@ -2,21 +2,6 @@ import numpy as np
 from agent import *
 from constants import MUTATION_CHANCE
 
-"""
-def mutation(agent:Agent, p):
-    chromosome = agent.chromosome
-    flat_genes = chromosome.flattened()
-    random_mutation = np.random.rand()
-    if random_mutation <= p.mutation_chance:
-        mutation_number = np.random.randint(1, int(flat_genes.size*p.mutation_chance))
-        for mutations in range(mutation_number):
-            random_index = np.random.randint(0, 2*9*5)
-            mutation_value = np.random.randint(-5000, 5000)
-            flat_genes[random_index] = mutation_value
-    new_chromosome = flat_genes.reshape(chromosome.shape)
-    return new_chromosome
-"""
-    
 
 def mutation(genes, p):
     for i in range(len(genes)):
